38651073b24b91043acb53e764b4e971ed1aa6a4
Katrina Peterson <katrina.peterson.95@gmail.com>
2019-04-09 12:38:11 -0600

    most recent

--- account/templates/__dmpcache__/prescriberdetail.html.py
+++ account/templates/__dmpcache__/prescriberdetail.html.py
@@ -5,14 +5,14 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1554833602.8145494
+_modified_time = 1554834698.0484362
 _enable_loop = True
 _template_filename = 'C:/Users/Katrina/Documents/INTEXII/thepack-intex/account/templates/prescriberdetail.html'
 _template_uri = 'prescriberdetail.html'
 _source_encoding = 'utf-8'
 import django_mako_plus
 import django.utils.html
-_exports = ['title', 'content']
+_exports = []
 
 
 def _mako_get_namespace(context, name):
@@ -30,51 +30,8 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        def title():
-            return render_title(context._locals(__M_locals))
-        form = context.get('form', UNDEFINED)
-        def content():
-            return render_content(context._locals(__M_locals))
-        self = context.get('self', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n\r\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'title'):
-            context['self'].title(**pageargs)
-        
-
-        __M_writer('\r\n\r\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
-            context['self'].content(**pageargs)
-        
-
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
-def render_title(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def title():
-            return render_title(context)
-        __M_writer = context.writer()
-        __M_writer('Dashboard')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
-def render_content(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        form = context.get('form', UNDEFINED)
-        def content():
-            return render_content(context)
-        self = context.get('self', UNDEFINED)
-        __M_writer = context.writer()
-        __M_writer('\r\n\r\n<form  method="post">\r\n  <table>\r\n  ')
-        __M_writer(django_mako_plus.ExpressionPostProcessor(self)( form.as_table() ))
-        __M_writer(' \r\n  </table>\r\n  <input type="submit" value="Submit">\r\n</form>\r\n\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -82,6 +39,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/Katrina/Documents/INTEXII/thepack-intex/account/templates/prescriberdetail.html", "uri": "prescriberdetail.html", "source_encoding": "utf-8", "line_map": {"29": 0, "40": 1, "45": 3, "55": 3, "61": 3, "67": 5, "75": 5, "76": 9, "77": 9, "83": 77}}
+{"filename": "C:/Users/Katrina/Documents/INTEXII/thepack-intex/account/templates/prescriberdetail.html", "uri": "prescriberdetail.html", "source_encoding": "utf-8", "line_map": {"29": 0, "34": 1, "40": 34}}
 __M_END_METADATA
 """
