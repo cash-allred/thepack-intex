dc7e8acfc5ef3736fe3c2a0559c3d4e8f9e6d79c
Katrina Peterson <katrina.peterson.95@gmail.com>
2019-04-08 22:00:27 -0600

    added models for doctor and drug

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+from decimal import Decimal
+TAX_RATE = Decimal("0.05")
+
+# Create your models here.
+
+class Doctor (models.Model):
+    firstName = models.TextField(default="Doctor First Name")
+    lastName = models.TextField(default="Doctor Last Name")
+        STATUS_CHOICES = [
+        ( 'F', 'Female' ),
+        ( 'M', 'Male' ),
+    ]
+    gender = models.CharField(max_length=1, default=STATUS_CHOICES[0][0], choices=STATUS_CHOICES)
+    credentials = models.TextField(default="Doctor Credentials")
+    location = models.TextField(default="Doctor Location")
+    specialty = models.TextField(default="Doctor Specialty")
+
+class Drug (models.Model):
+    name = models.TextField(default="Drug Name")
+        STATUS_CHOICES = [
+            ('T' = 'Is Opiate'),
+            ('F' = 'Is Not Opiate'),
+        ]
+    drugType = models.CharField(max_lenth=1, default=STATUS_CHOICES[0][0], choices=STATUS_CHOICES)
+    prescriber = models.ForeignKey(Doctor, on_delete = models.CASCADE)
+    quantity = models.DecimalField(max_digits=20, decimal_places=6)
+
+
+
+
+
+
+
