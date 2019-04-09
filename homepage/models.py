04b9bb00148754883393e2afd3f44d242be7c283
Katrina Peterson <katrina.peterson.95@gmail.com>
2019-04-09 11:29:58 -0600

    added od deaths and have all the columns in order to match the csv files

--- homepage/models.py
+++ homepage/models.py
@@ -10,22 +10,22 @@ TAX_RATE = Decimal("0.05")
 # Create your models here.
 
 class Doctor (models.Model):
-    firstName = models.TextField(default="Doctor First Name")
-    lastName = models.TextField(default="Doctor Last Name")
-    doctorID = models.IntegerField(default="0")
+    doctorID = models.IntegerField(default=0)
+    fName = models.TextField(default="Doctor First Name")
+    lName = models.TextField(default="Doctor Last Name")
     STATUS_CHOICES = [
         ( 'F', 'Female' ),
         ( 'M', 'Male' ),
     ]
     gender = models.CharField(max_length=1, default=STATUS_CHOICES[0][0], choices=STATUS_CHOICES)
-    credentials = models.TextField(default="Doctor Credentials")
-    location = models.TextField(default="Doctor Location")
+    state = models.TextField(default="Doctor Location")
+    credentials = models.TextField(null = true)
     specialty = models.TextField(default="Doctor Specialty")
     OPIOID_CHOICES = [
-        ( 'F', 'False' ),
-        ( 'T', 'True' ),
+        ( 0, 'False' ),
+        ( 1, 'True' ),
     ]
-    opioidPrescriber = models.CharField(max_length=1, default=OPIOID_CHOICES[0][0], choices=OPIOID_CHOICES)
+    opioidPrescriber = models.IntegerField(default=OPIOID_CHOICES[0][0])
     totalPrescriptions = models.IntegerField()
 
 class Drug (models.Model):
@@ -39,7 +39,14 @@ class Drug (models.Model):
 class Prescription(models.Model):
     doctorID = models.ForeignKey(Doctor, on_delete = models.CASCADE)
     drugName = models.ForeignKey(Drug, on_delete = models.CASCADE)
-    quantity = models.DecimalField(max_digits=20, decimal_places=6)
+    quantity = models.IntegerField(default=0)
+
+class OverdoseDeaths(models.Model):
+    state = models.TextField(default="State")
+    population = models.IntegerField(default=0)
+    deaths = models.IntegerField(default=0)
+    abbrev = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
 
 
 
