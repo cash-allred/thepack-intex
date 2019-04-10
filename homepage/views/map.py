7747b7b0bd769a82704dcefd0a4adde4884c6ae3
ParkerFrame <parkerframe8@gmail.com>
2019-04-10 15:39:55 -0600

    added the tableau dashboard and map

---
+++ homepage/views/map.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django_mako_plus import view_function, jscontext
+from datetime import datetime, timezone
+from homepage import models as hmod
+import math
+from django.core.exceptions import ValidationError
+from django import forms
+from django.shortcuts import render, HttpResponseRedirect
+
+@view_function
+def process_request(request):
+    context = {
+        jscontext('now'): datetime.now(),
+    }
+    return request.dmp.render('map.html', context)
