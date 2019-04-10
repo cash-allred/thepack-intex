623eed58c14dddc48058d345f6dfde8ff2740432
Katrina Peterson <katrina.peterson.95@gmail.com>
2019-04-10 15:59:30 -0600

    forgot to save - this includes new changes

--- homepage/views/updateDoctor.py
+++ homepage/views/updateDoctor.py
@@ -2,66 +2,46 @@ from django.conf import settings
 from django_mako_plus import view_function, jscontext
 from datetime import datetime, timezone
 from homepage import models as hmod
+from django.db.models import Avg
 import math
 from django.core.exceptions import ValidationError
 from django import forms
 from django.shortcuts import render, HttpResponseRedirect
 
-#CREATE
-#READ: reading and searching capability is taken care of with the @view_function process request
-#UPDATE
-#DELETE
 
-
-#figure out create function!
 @view_function
-def process_request(request):
-    doctor = hmod.Doctor
-    if request.method =="POST":
-        form=doctorCreateForm(request.POST)
-        form.doctor = doctor
-        form.user = request.user
-        #if not form.user.is_authenticated:
-            #return HttpResponseRedirect('/account/login/')
-        if form.is_valid():
-            form.commit()
-            return HttpResponseRedirect('/')
-        form = doctorCreateForm()
+def process_request(request, docid):
     
+    if request.method == "POST":
+    #clean the form and stuff, remove the line of code below
+        form=doctorSearchForm(request.POST)
+        if form.is_valid():
+            doctors = hmod.Doctor.objects.filter(fName__contains=form.cleaned_data['firstName'])
+            doctors = doctors.filter(lName__contains=form.cleaned_data['lastName'])
+            doctors = doctors.filter(gender__contains=form.cleaned_data['gender'])
+            doctors = doctors.filter(state__contains=form.cleaned_data['state'])
+            doctors = doctors.filter(credentials__contains=form.cleaned_data['credentials'])
+            doctors = doctors.filter(specialty__contains=form.cleaned_data['specialty'])
     else:
-        form = doctorCreateForm()
-        
-    context = {
-        'doctor': doctor,
-        'form': form,
-        jscontext('now'): datetime.now(),
+        form=updateDoctorForm() = ""#hmod.Doctor.objects.all()
+    #numpages = math.ceil(doctors.count()/ ITEMS_PER_PAGE)
+    #doctors = doctors[(page-1) * ITEMS_PER_PAGE: page * ITEMS_PER_PAGE]
+    form = doctorSearchForm() 
+    context={
+        'doctors': doctors,
+        'form':form,
+        #'page': page,
+        #'numpages': numpages,
     }
-    return request.dmp.render('createDoctor.html', context)
+    return request.dmp.render('index.html', context)
 
-class doctorCreateForm(forms.Form):
-    FirstName = forms.CharField(widget=forms.TextInput, label='First Name', required=True)
-    LastName = forms.CharField(widget=forms.TextInput, label='Last Name', required=True)
-    Gender = forms.ChoiceField(choices=hmod.Doctor.STATUS_CHOICES)
-    State = forms.CharField(widget=forms.TextInput, required=True)
-    Credentials = forms.ChoiceField(choices=[('MD','MD'),('DO','DO'),('DMD','DMD'),('NP','NP'),('DDS','DDS'),('PA','PA'),('MED','MED'),('LPC','LPC'),('RN','RN'),('OD','OD'),('','Null')], required=True)
-    Specialty = forms.CharField(widget=forms.TextInput, required=True)
-    OpioidPrescriber = forms.ChoiceField(choices=hmod.Doctor.OPIOID_CHOICES, label='Opioid Prescriber')
+class doctorSearchForm(forms.Form):
+    firstName = forms.CharField(widget=forms.TextInput, label='First Name', required=False)
+    lastName = forms.CharField(widget=forms.TextInput, label='Last Name', required=False)
+    gender = forms.ChoiceField(choices=[('M','Male'),('F','Female'),('','Null')], required=False)
+    credentials = forms.ChoiceField(choices=[('MD','MD'),('DO','DO'),('DMD','DMD'),('NP','NP'),('DDS','DDS'),('PA','PA'),('MED','MED'),('LPC','LPC'),('RN','RN'),('OD','OD'),('','Null')], required=False)
+    state = forms.CharField(widget=forms.TextInput, required=False)
+    specialty = forms.CharField(widget=forms.TextInput, required=False)
 
     def clean(self):
         return self.cleaned_data
-
-    def commit(self):
-        newDoc = hmod.Doctor()
-        newDoc.doctorID = 0
-        newDoc.fName = self.cleaned_data.get('FirstName')
-        newDoc.lName = self.cleaned_data.get('LastName')
-        newDoc.gender = self.cleaned_data.get('Gender')
-        newDoc.state = self.cleaned_data.get('State')
-        newDoc.credentials = self.cleaned_data.get('Credentials')
-        newDoc.opioidPrescriber = self.cleaned_data.get('OpioidPrescriber')
-        newDoc.totalPrescriptions = 0
-
-        newDoc.save()
-
-
-    
